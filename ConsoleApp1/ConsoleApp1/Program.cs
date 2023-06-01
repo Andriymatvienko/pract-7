fce7b38ee827e89b0232164bf57e0859e5f0c21e
Andriymatvienko <130711488+Andriymatvienko@users.noreply.github.com>
2023-06-02 00:25:21 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿class Triangle
+{
+    private int[,] verticles;
+    public Triangle(int[,] verticles)
+
+    {
+        if (verticles.GetLength(0) != 3)
+        {
+            throw new ArgumentException("Трикутник повинен мати три вершини");
+        }
+        this.verticles = verticles;
+
+    }
+    public int this[int i]
+    {
+        get
+        {
+            int j = (i + 1) % 3;//індекс наст.вершини
+
+            return Math.Abs(verticles[i, 0] - verticles[j, 0] + Math.Abs(verticles[i, 1] - verticles[j, 1]));
+        }
+    }
+    public void InputVerticles()
+    {
+        Console.WriteLine("Введіть координато вершин трикутника:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Вершина {0}:", i + 1);
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+
+            verticles[i, 0] = x;
+            verticles[i, 1] = y;
+        }
+    }
+    public void OutputVerticles()
+    {
+        Console.WriteLine("Координати вершин трикутника:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Вершина {0}: ({1},{2})", i + 1, verticles[i, 0], verticles[i, 1]);
+
+        }
+    }
+    public int Perimeter()
+    {
+        int perimeter = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            perimeter += this[i];
+        }
+        return perimeter;
+    }
+    public double Area()
+    {
+        double area = (verticles[0, 0] * (verticles[1, 1] - verticles[2, 1]) + verticles[1, 0] * (verticles[2, 1] - verticles[0, 1]) + verticles[2, 0] * (verticles[0, 1] - verticles[1, 1])) / 2;
+        return Math.Abs(area);
+    }
+}
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[,] verticles = new int[3, 2];
+            Triangle triangle = new Triangle(verticles);
+
+            triangle.InputVerticles();
+            triangle.OutputVerticles();
+
+            int side1 = triangle[0];
+            int side2 = triangle[1];
+            int side3 = triangle[2];
+
+            Console.WriteLine("Сторона 1: {0}", side1);
+            Console.WriteLine("Сторона 1: {0}", side2);
+            Console.WriteLine("Сторона 1: {0}", side3);
+
+            int perimeter = triangle.Perimeter();
+            double area = triangle.Area();
+
+            Console.WriteLine("Периметр:{0}", perimeter);
+            Console.WriteLine("Площа: {0}", area);
+        }
+    }
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Rectangle
+{
+    private double length;
+    private double width;
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index == 0)
+                return length;
+            else if (index == 1)
+                return width;
+            else
+                throw new IndexOutOfRangeException("Invalid index");
+        }
+        set
+        {
+            if (index == 0)
+                length = value;
+            else if (index == 1)
+                width = value;
+            else
+                throw new IndexOutOfRangeException("Invalid index");
+        }
+    }
+
+    public double Perimeter
+    {
+        get { return 2 * (length + width); }
+    }
+
+    public double Area
+    {
+        get { return length * width; }
+    }
+
+    public void InputDimensions()
+    {
+        Console.WriteLine("Enter length: ");
+        length = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter width: ");
+        width = double.Parse(Console.ReadLine());
+    }
+
+    public void DisplayDimensions()
+    {
+        Console.WriteLine("Length: " + length);
+        Console.WriteLine("Width: " + width);
+    }
+
+    public void DisplaySides()
+    {
+        Console.WriteLine("Sides:");
+        Console.WriteLine("Side 1: " + this[0]);
+        Console.WriteLine("Side 2: " + this[1]);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle();
+        rectangle.InputDimensions();
+
+        Console.WriteLine("Perimeter: " + rectangle.Perimeter);
+        Console.WriteLine("Area: " + rectangle.Area);
+
+        rectangle.DisplaySides();
+
+        Console.ReadLine();
+    }
+}
